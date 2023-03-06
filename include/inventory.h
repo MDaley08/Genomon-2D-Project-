895885f13cdf5c24dd840fde86dd42e9e2a6d277
Michael Daley <mich@michaels-air.resource.campus.njit.edu>
2023-03-06 16:47:16 -0500

    updated team creation, began implementing inventory system

---
+++ include/inventory.h
@@ -0,0 +1,23 @@
+#ifndef __INVENTORY_H__
+#define __INVENTORY_H__
+
+#include "gfc_types.h"
+#include "item.h"
+
+const Uint8 MAX_INV_SIZE = 20;
+typedef struct
+{
+    Bool    _inUse;
+    Item    *item;
+}InvSlot;
+
+typedef struct
+{
+    InvSlot slots[MAX_INV_SIZE];
+}Inventory;
+
+
+
+
+
+#endif
